2eb4770ed4e4a5332e36cb6494647cddf3c28e53
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-05-16 16:42:02 -0700

    Update LapsInstall.bat

--- LapsInstall.bat
+++ LapsInstall.bat
@@ -1,4 +1,4 @@
-itsadmin.exe /transfer "Laps" https://github.com/murtaza7869/JohnHopkns/raw/main/LAPS.x64.msi %WinDir%\LAPS.x64.msi
+bitsadmin.exe /transfer "Laps" https://github.com/murtaza7869/JohnHopkns/raw/main/LAPS.x64.msi %WinDir%\LAPS.x64.msi
 MSIEXEC.EXE /I "LAPS.x64.msi" /qn /l* %WinDir%\LAPSx64-6.2_Client-Install.log
 set InstallErrorLevel=%ERRORLEVEL%
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\GPExtensions\{D76B9641-3288-4f75-942D-087DE603E3EA}" /v "ExtensionDebugLevel" /t REG_DWORD /d 00000001 /reg:64 /f
